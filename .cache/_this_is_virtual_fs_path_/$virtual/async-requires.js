4ffd9a366c3f6e65bf6c9c70c99fbe2a00fbf1f6
Johanna Lee <johannalee@berkeley.edu>
2024-10-08 11:29:42 -0700

    updating favicon

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,9 +1,16 @@
 exports.components = {
-  "component---src-pages-broke-js": () => import("./../../../src/pages/Broke.js" /* webpackChunkName: "component---src-pages-broke-js" */),
-  "component---src-pages-es-net-mrdp-js": () => import("./../../../src/pages/ESNetMRDP.js" /* webpackChunkName: "component---src-pages-es-net-mrdp-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-sia-partners-js": () => import("./../../../src/pages/SiaPartners.js" /* webpackChunkName: "component---src-pages-sia-partners-js" */),
-  "component---src-pages-waymo-js": () => import("./../../../src/pages/Waymo.js" /* webpackChunkName: "component---src-pages-waymo-js" */),
-  "component---src-pages-yelp-js": () => import("./../../../src/pages/Yelp.js" /* webpackChunkName: "component---src-pages-yelp-js" */)
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-broke-js": () => import("./../../../src/pages/Broke.js?export=default" /* webpackChunkName: "component---src-pages-broke-js" */),
+  "component---src-pages-es-net-mrdp-js": () => import("./../../../src/pages/ESNetMRDP.js?export=default" /* webpackChunkName: "component---src-pages-es-net-mrdp-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-sia-partners-js": () => import("./../../../src/pages/SiaPartners.js?export=default" /* webpackChunkName: "component---src-pages-sia-partners-js" */),
+  "component---src-pages-waymo-js": () => import("./../../../src/pages/Waymo.js?export=default" /* webpackChunkName: "component---src-pages-waymo-js" */),
+  "component---src-pages-yelp-js": () => import("./../../../src/pages/Yelp.js?export=default" /* webpackChunkName: "component---src-pages-yelp-js" */)
+}
+
+
+
+exports.head = {
+
 }
 
