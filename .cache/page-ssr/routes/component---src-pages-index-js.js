07c635abba2842d6a91dcd322f4b0735262bd272
Johanna Lee <johannalee@berkeley.edu>
2025-02-21 12:22:37 -0800

    updated stuff

--- .cache/page-ssr/routes/component---src-pages-index-js.js
+++ .cache/page-ssr/routes/component---src-pages-index-js.js
@@ -4845,7 +4845,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/lopster-9c5d51d72547f351d3074957f0a0e510.png");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/lopster-9b0d392420c3e0cdb15ea8364f8ae52f.png");
 
 /***/ }),
 
@@ -4860,7 +4860,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/profileDraw-49038bda35e26083ab7fa1c8e836fe4b.png");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/profileDraw-5a1d611b7c590cdae1fec72a0b0ad618.png");
 
 /***/ }),
 
--- .cache/page-ssr/routes/component---src-pages-waymo-js.js
+++ .cache/page-ssr/routes/component---src-pages-waymo-js.js
@@ -4661,7 +4661,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/lopster-9c5d51d72547f351d3074957f0a0e510.png");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/static/lopster-9b0d392420c3e0cdb15ea8364f8ae52f.png");
 
 /***/ }),
 
